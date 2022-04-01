35f378d8c589af4f788c86db7262aa9a6b03ad2e
Junyeop Kim <junykim@c10r4s8.42seoul.kr>
2022-04-01 18:31:31 +0900

    second

--- get_next_line.c
+++ get_next_line.c
@@ -6,50 +6,54 @@
 /*   By: junykim <junykim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/31 20:11:08 by junykim           #+#    #+#             */
-/*   Updated: 2022/04/01 15:17:07 by junykim          ###   ########.fr       */
+/*   Updated: 2022/04/01 18:30:10 by junykim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-// 바이트가 0보다 크면 개행까지 읽어서 반환해주면 그 뒤에 있는 값은 어떻게 되는거?
-// 개행을 찾았으면 다음 값의 주소를 반환하게?
+/* if there LF is middle of the buffer, using static cause save character after LF */
+/* static char *line : have to return 'line include \n'  */
 // find memory leak
-//
+// if func return buf , means there is no \n so have to take next line in GNL
+//할당이 안됐는데, 프리하는 경우
+//free 한 후, 댕글링 포인터를 프리하는 경우
+/* fd : save pos where it's finished.*/
 #define BUFFER_SIZE 32//임시용
 static char	*find_LF_return_line(char *buf)
 {
-	char			*LF;
+	static char		*after_LF;
 	unsigned int	start;
 
 	start = 0;
-	LF = ft_strchr(buf, '\n');//개행의 주소를 담기
-	while (buf)
+	while (buf[start])
 	{
-		if (&buf[start++] == LF)
+		if (ft_strchr(buf, '\n') != NULL)
 			break ;
+		start++;
 	}
-	return (ft_substr(buf, start, buf - LF + 1));
+	if (!buf[start])
+		return (buf);
+	after_LF = ft_substr(buf, start, ft_strlen(buf) - start);
+	return (ft_substr(buf, 0, start));
 }
 
 char	*get_next_line(int fd)
 {
-	char	*buf;
-	char	*line;
-	int		byte;
+	char		buf[BUFFER_SIZE + 1];//is this static too?
+	static char	*line;
+	int			byte;
 
-	buf = malloc(sizeof(char) * BUFFER_SIZE + 1);
-	if (!buf)
-		return (0);
+	byte = read(fd, buf, BUFFER_SIZE);// 이러면 fd의 값은 3으로 고정이고 가리키는 주소만 바뀌는거?
 	buf[BUFFER_SIZE] = 0;
-	byte = read(fd, buf, BUFFER_SIZE);
-	while (1)
+	while (byte > 0 && ft_strchr(line,'\n') == NULL)
 	{
-		if (byte < 0)
-			return (0);
-		if (byte == 0)
-			return (ft_strdup(""));
 		line = find_LF_return_line(buf);
+		byte = read(fd, buf, BUFFER_SIZE);
+		buf[BUFFER_SIZE] = 0;
 	}
+	if (byte < 0)
+		return (NULL);
+	if (byte == 0)
+		return (ft_strdup(""));
 	return (line);
-
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: junykim <junykim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/31 20:20:14 by junykim           #+#    #+#             */
-/*   Updated: 2022/04/01 15:22:43 by junykim          ###   ########.fr       */
+/*   Updated: 2022/04/01 15:35:52 by junykim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,5 @@ char	*ft_substr(char const *s, unsigned int start, size_t n);
 char	*ft_strdup(const char *s);
 size_t	ft_strlen(const char *s);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
+char	*get_next_line(int fd);
 #endif
